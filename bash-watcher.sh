2d3dc11be42c6e2c4446d0e5424fda5708b35816
Ruwan Ranganath <ruwanranganathz@gmail.com>
2019-05-09 12:47:09 +0530

    init

---
+++ bash-watcher.sh
@@ -0,0 +1,7 @@
+# Need to install `apt install inotify-tools`
+
+inotifywait -m /path -e create -e moved_to |
+    while read path action file; do
+        echo "The file '$file' appeared in directory '$path' via '$action'"
+        # do something with the file
+    done
